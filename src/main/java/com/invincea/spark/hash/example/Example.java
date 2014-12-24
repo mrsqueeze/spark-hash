0a19a1394175a7946db7a683de6de5f9a3d17eb0
Mike Orr <morr@derecho.io>
2014-12-23 19:31:34 -0500

    needed for sonatype javadoc

---
+++ src/main/java/com/invincea/spark/hash/example/Example.java
@@ -0,0 +1,11 @@
+package com.invincea.spark.hash.example;
+
+
+/**
+ * This class is just used to generate javadocs
+ * https://issues.sonatype.org/browse/OSSRH-637
+ */
+public class Example {
+
+}
+
